c316ab573efa8e82125bdf0cf56526e9d4258099
Parnell <psteide@ford.com>
2022-05-11 15:11:50 -0400

    Add active class

---
+++ src/app/list-of-links.ts
@@ -0,0 +1,8 @@
+import { Link } from './link'
+
+export const navLinks: Link[] = [
+  { name: 'Home', isActive: true },
+  { name: 'Whites', isActive: false },
+  { name: 'Home', isActive: false },
+  { name: 'Home', isActive: false },
+]
--- src/app/primary-navigation/primary-navigation.component.ts
+++ src/app/primary-navigation/primary-navigation.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from '../link';
 
 @Component({
   selector: 'app-primary-navigation',
   templateUrl: './primary-navigation.component.html',
-  styleUrls: ['./primary-navigation.component.css']
+  styleUrls: ['./primary-navigation.component.css'],
 })
 export class PrimaryNavigationComponent implements OnInit {
+  navLinks: Link[] = [
+    { name: 'Home', isActive: true },
+    { name: 'Whites', isActive: false },
+    { name: 'Reds', isActive: false },
+    { name: 'Blends', isActive: false },
+  ];
 
-  constructor() { }
+  activeLink?: Link
+
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    this.navLinks.forEach((link) => {
+      if(link.isActive === true){
+        this.activeLink = link
+      }
+    })
 
+  }
 }
